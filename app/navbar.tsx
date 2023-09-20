8b9ed6e7196d1a4c9af7e15e0b5038a089d150c8
Minh Hải <champion.minh@gmail.com>
2023-09-20 08:15:49 +0000

    Update navbar.tsx

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -113,7 +113,7 @@ export default function Navbar({ user }: { user: any }) {
                                 active ? 'bg-gray-100' : '',
                                 'flex w-full px-4 py-2 text-sm text-gray-700'
                               )}
-                              onClick={() => signIn('google')}
+                              onClick={() => signIn()}
                             >
                               Sign in
                             </button>
